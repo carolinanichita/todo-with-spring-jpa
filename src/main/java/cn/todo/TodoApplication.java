e614f390ac0062c8f9f4d89c4993d9d7b384bb45
Carolina Nichita <carolinanichita89@gmail.com>
2024-03-25 14:45:26 +0100

    Customize Resource JSON Output using RepresentationModelProcessor interface

--- src/main/java/cn/todo/TodoApplication.java
+++ src/main/java/cn/todo/TodoApplication.java
@@ -1,8 +1,14 @@
 package cn.todo;
 
+import cn.todo.controllers.TodoRestController;
+import cn.todo.domains.Todo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.Bean;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelProcessor;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @SpringBootApplication
 public class TodoApplication {
@@ -11,4 +17,14 @@ public class TodoApplication {
 		SpringApplication.run(TodoApplication.class, args);
 	}
 
+	@Bean
+	public RepresentationModelProcessor<EntityModel<Todo>> todoProcessor() {
+		return new RepresentationModelProcessor<EntityModel<Todo>>() {
+			@Override
+			public EntityModel<Todo> process(EntityModel<Todo> model) {
+				model.add(linkTo(methodOn(TodoRestController.class).todoTypes()).withRel("todoTypes"));
+				return model;
+			}
+		};
+	}
 }
